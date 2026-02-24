fe852c70b6ddac141823c87cb3fa809c5f9a6294
Nosvetra <goludhruvkumar@gmail.com>
2026-02-24 17:11:38 +0530

    installed pacakges

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 import express from "express";
+import pg from "pg";
 import bodyParser from "body-parser";
 import moduleName from "axios";
 import env from "dotenv";
 
 const app = express();
 const port = process.env.PORT || 5000;
+const db = new pg.Client({
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+});
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
